6da74eaaa8dfebbc79c0fb919d9cf608ca5ef04e
Tarique Ejaz <tariqueejaz.93102@gmail.com>
2021-09-24 14:24:27 +0200

    feat(starter): adding starter files

---
+++ helloworld/main.go
@@ -0,0 +1,17 @@
+/*
+	Fundamental Questions
+
+	1. How do we run the code in our project?
+	2. What does 'package main' mean?
+	3. What does 'import "fmt"' mean?
+	4. What's that 'func' thing?
+	5. How is the main.go file organized?
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hi there!")
+}
