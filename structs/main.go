d44bc1bef7053c83e0ad607ac6f2d735666e35f3
Tarique Ejaz <tariqueejaz.93102@gmail.com>
2021-09-29 15:52:52 +0200

    feat: map overview

--- structs/main.go
+++ structs/main.go
@@ -31,8 +31,8 @@ func main() {
 		},
 	}
 
-	jimPointer := &jim // & => sets the memory address of the variable
-	jimPointer.updateName("Pam")
+	// & => sets the memory address of the variable
+	jim.updateName("Pam")
 	jim.print()
 }
 
